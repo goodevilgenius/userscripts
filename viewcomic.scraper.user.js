4321c75ddd038b0775865daa4d3ed2b1cdc54d82
Dan Jones <dan@medology.com>
2018-01-19 13:02:27 -0600

    Add viewcomic scraper

---
+++ viewcomic.scraper.user.js
@@ -0,0 +1,39 @@
+// -*- tab-width: 4; js-indent-level: 4; -*-
+// ==UserScript==
+// @name         Viewcomic Scraper
+// @namespace    danielrayjones
+// @version      0.0.1
+// @description  Scrape comics from viewcomic.com
+// @author       Dan Jones
+// @match        http://viewcomic.com/*
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @require      https://raw.githubusercontent.com/tommcfarlin/konami-code/master/src/jquery.konami.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $(window).konami({
+        code: [71,69,84],
+        eventName: 'konami.get'
+    });
+
+    $(window).on('konami.get', getStuff);
+
+    function getStuff() {
+      let i = 0;
+
+      $('div.separator a img').each(function () {
+        let $el = $('<a>');
+        $(document.body).append($el);
+
+        $el.attr('href', this.src);
+        $el.attr('download', (i < 10 ? '00' : '0' ) + i + '.jpg');
+        $el.get(0).click();
+
+        i = i+1;
+      });
+    }
+
+})();
