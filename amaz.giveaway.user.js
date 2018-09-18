95f1340722ab24c0f701b20697f04fc454fc8f49
Dan Jones <dan@raven.com>
2018-09-18 09:08:43 -0500

    ⬆ Update for new layout

--- amaz.giveaway.user.js
+++ amaz.giveaway.user.js
@@ -4,59 +4,66 @@
 // @description Allow removal of visited links on Amazon Giveaway Listing
 // @include https://smile.amazon.com/ga/giveaways*
 // @include https://www.amazon.com/ga/giveaways*
-// @version 1.8.0
+// @version 1.9.0
 // @require https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // ==/UserScript==
 
-var checked = false;
-var regex_hide;
+/* global jQuery */
 
-var getSmallUrl = function(fullUrl) {
-  return fullUrl.split('?')[0];
-};
+(function ($) {
+  'use strict';
 
-var hideOne = function(el) {
-  $(el).parents('.giveawayItemContainer').css('display', checked ? 'none' : 'block');
-};
+  var checked = false;
+  var regex_hide;
 
-var hideVisited = function(evt) {
-  checked = $(this).prop('checked');
-  var $links = $('#giveaway-grid a[href*="amazon.com/ga"]');
+  var getSmallUrl = function(fullUrl) {
+    return fullUrl.split('?')[0];
+  };
 
-  var hide = regex_hide ? new RegExp(regex_hide, 'i') : null;
+  var hideOne = function(el) {
+    $(el).parents('.listing-item').css('display', checked ? 'none' : 'block');
+  };
 
-  $links.each(function() {
-    var href = getSmallUrl($(this).attr('href'));
-    if (localStorage.getItem(href)) {
+  var hideVisited = function(evt) {
+    checked = $(this).prop('checked');
+    var $links = $('.listing-info-container a[href*="/ga"]');
+
+    var hide = regex_hide ? new RegExp(regex_hide, 'i') : null;
+
+    $links.each(function() {
+      var href = getSmallUrl($(this).attr('href'));
+      if (localStorage.getItem(href)) {
+        hideOne(this);
+      }
+
+      var title = $(this).find('.prize-title').text();
+      if (hide && title && hide.test(title)) {
+        hideOne(this);
+      }
+    });
+  };
+
+  $(document).ready(function() {
+    //$('head').append('<base target="_blank" />');
+
+    $('.listing-info-container').on('click', 'a[href*="/ga"]', function(evt) {
+
+      var href = getSmallUrl($(this).attr('href'));
+      localStorage[href] = "visited";
       hideOne(this);
-    }
+    });
 
-    var title = $(this).find('.giveawayPrizeNameContainer').text();
-    if (hide && title && hide.test(title)) {
-      hideOne(this);
-    }
-  });
-};
+    regex_hide = localStorage.getItem('regex_hide') || '';
 
-$(document).ready(function() {
-  $('head').append('<base target="_blank" />');
+    $('#giveaway-numbers-container')
+      .append('<label><input type="checkbox" id="hide_visited"/> Visited</label>')
+      .append(`<input id="hide_regex" value="${regex_hide}"/>`);
 
-  $('#giveaway-grid').on('click', 'a[href*="amazon.com/ga"]', function(evt) {
-
-    var href = getSmallUrl($(this).attr('href'));
-    localStorage[href] = "visited";
-    hideOne(this);
+    $('#hide_visited').on('click', hideVisited);
+    $('#hide_regex').on('change', function() {
+      regex_hide = $(this).val();
+      localStorage.regex_hide = regex_hide;
+    });
   });
 
-  regex_hide = localStorage.getItem('regex_hide') || '';
-
-  $('#giveaway-result-info-bar-content')
-    .append('<label><input type="checkbox" id="hide_visited"/> Visited</label>')
-    .append(`<input id="hide_regex" value="${regex_hide}"/>`);
-
-  $('#hide_visited').on('click', hideVisited);
-  $('#hide_regex').on('change', function() {
-    regex_hide = $(this).val();
-    localStorage.regex_hide = regex_hide;
-  });
-});
+})(jQuery);
